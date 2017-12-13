2ef72b773cd22ec34a929e716aa0649a269e61ea
Shinfreyr <fh.lamodiere@gmail.com>
2017-12-13 14:21:56 +0100

    Ajout de la Boucle de création du carré

--- exercice.js
+++ exercice.js
@@ -5,3 +5,33 @@ var j=0;
 
 var tableauFs=[]; // char[][]
 var tableauLs=[]; // char[]
+
+//Boucle de création du carré
+while (i<nombreN){
+    //première et dernière ligne du carré
+    if(i===0||i===nombreN-1){
+        j=0;
+        while(j<nombreN){
+            tableauLs.push("#");
+            j++;
+        }
+    }
+    //autres lignes
+    else{
+        j=0;
+        while(j<nombreN){
+            if(j===0||j===nombreN-1){
+                tableauLs.push("#");
+                j++;
+            }
+            else{
+                tableauLs.push("*");
+                j++;
+            }
+        }
+    }
+    //envois du tableau Ligne dans le tableau final
+    tableauFs.push(tableauLs);
+    tableauLs=[];
+    i++;
+}
